84e7af3897d83ab39e0efeb9300fde0e860dda3d
Des Whelan <wheland6@tcd.ie>
2019-03-29 11:51:13 +1300

    add routes for all pages

--- routes.js
+++ routes.js
@@ -1,46 +1,35 @@
-
-// const cohort = require('./data').people
-// const randomizer = require('./randomizer')
-
-// const teams = for (var i = 0; i < cohort.length)
-
-//   router.get('/profiles/:id', (req, res) => {
-//     let id = req.params.id;
-//     let cohort = people.find(x => x.id == id)
-//     res.render('ids/detail', cohort)
-//   })
-
-// router.get('/profiles', (req, res) => {
-//   const viewData = {
-//     cohort: cohort
-//   }
-//   res.render('ids/index', viewData)
-// })
-
-// router.get('/teams', (req, res) => {
-//   const viewData = { teams }
-
-//   res.render('ids/teams', viewData)
-// })
-
-
-
-// let teams = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12][13, 14, 15, 16]];
-
-// for (var i = 0; i < teams.length; i++) {
-//   for (var j = 0; j < teams[i][j]; j++) {
-//     teams[i][j]
-//   }
-// }
-
 const express = require('express')
 const router = express.Router()
-const data = require('./data')
+const people = require('./data').people
+const randomizer = require('./randomizer')
 const fs = require('fs')
+
+let teams = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12][13, 14, 15, 16]];
+
+router.get('/profiles', (req, res) => {
+  const viewData = {
+    people: people
+  }
+  res.render('ids/index', viewData)
+})
+
+router.get('/profiles/:id', (req, res) => {
+  let id = req.params.id;
+  let cohort = people.find(x => x.id == id)
+  res.render('ids/detail', cohort)
+})
+
+router.get('/teams', (req, res) => {
+  const viewData = { teams }
+
+  res.render('ids/teams', viewData)
+})
 
 router.use('/', (req, res) => {
   res.send("hi there")
 })
 
 module.exports = router;
+
+
 
