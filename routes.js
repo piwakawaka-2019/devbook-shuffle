f000f1deba932e6e20c7e1a40ae3954e95f8f2dc
mktsewhoy <mktsewhoy@hotmail.co.nz>
2019-03-29 10:39:00 +1300

    working copy

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const data = require('./data')
 const fs = require('fs')
 
-router.get('/', (req,res) => {
-    res.render('views/layouts/main', data);
+router.use('/', (req, res) => {
+  res.send("hi there")
 })
+module.exports = router;
